0195db8d3776f23b8903bd9755d1a21f85420261
rishi <rchander@med.umich.edu>
2019-01-31 07:12:27 -0500

    updated deliverable 2 to include an option to re-enter if the user enters an uniterpretable entry

--- rishichanderraj_deliverable2.cs
+++ rishichanderraj_deliverable2.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace deliverable2
+namespace ConsoleApp4
 {
     class Program
     {
@@ -16,61 +16,74 @@ namespace deliverable2
             Console.WriteLine("2) Chill Times");
             Console.WriteLine("3) Danger");
             Console.WriteLine("4) Good Food");
+            
+            bool correctResponse1 = false;
+            while (correctResponse1 == false)
+            {
+                int entry1 = Convert.ToInt16(Console.ReadLine());
 
-            int entry1 = Convert.ToInt16(Console.ReadLine());
-         
-             switch (entry1)
+                switch (entry1)
                 {
                     case 1:
                         userAnswers[0] = "action";
                         userAnswers[1] = "go stock car racing";
+                        correctResponse1 = true;
                         break;
                     case 2:
                         userAnswers[0] = "chill times";
                         userAnswers[1] = "go hiking";
+                        correctResponse1 = true;
                         break;
                     case 3:
                         userAnswers[0] = "danger";
                         userAnswers[1] = "skydiving";
+                        correctResponse1 = true;
                         break;
                     case 4:
                         userAnswers[0] = "good food";
                         userAnswers[1] = "go to Taco Bell";
+                        correctResponse1 = true;
                         break;
                     default:
-                        Console.WriteLine("I didn't understand your response");
+                        Console.WriteLine("I didn't understand your response please enter an integer 1-4");
+                        correctResponse1 = false;
                         break;
                 }
-                Console.WriteLine("How many people are you bringing with you?");
-                int entry2 = Convert.ToInt16(Console.ReadLine());
+            }
 
+            Console.WriteLine("How many people are you bringing with you?");
+            
+            bool correctResponse2 = false;
+            while (correctResponse2 == false)
+            {
+                int entry2 = Convert.ToInt16(Console.ReadLine());
                 switch (entry2)
                 {
-                    case int n when (n < 1):
+                    
+                    case int n when (n < 1 && n > -1):
                         userAnswers[2] = "some sneakers";
+                        correctResponse2 = true;
                         break;
                     case int n when (n >= 1 && n < 5):
                         userAnswers[2] = "a sedan";
+                        correctResponse2 = true;
                         break;
                     case int n when (n >= 5 && n <= 10):
                         userAnswers[2] = "a Volkswagen bus";
+                        correctResponse2 = true;
                         break;
                     case int n when (n >= 11):
                         userAnswers[2] = "an airplane";
+                        correctResponse2 = true;
                         break;
                     default:
-                        Console.WriteLine("I didn't understand your response");
+                        Console.WriteLine("I didn't understand your response please enter an integer value greater than 0");
                         break;
                 }
-            if (userAnswers[0] == null || userAnswers[2] == null)
-            {
-                Console.WriteLine("The program didn't understand your responses, you'll have to try again");
             }
-            else
-            {
-                Console.WriteLine("Okay!  If you're in the mood for {0} then you should {1} and travel in {2}", userAnswers[0], userAnswers[1], userAnswers[2]);
-                Console.WriteLine("Goodbye");
-            }
+            Console.WriteLine("Okay!  If you're in the mood for {0} then you should {1} and travel in {2}", userAnswers[0], userAnswers[1], userAnswers[2]);
+            Console.WriteLine("Goodbye");
+
         }
     }
 }
